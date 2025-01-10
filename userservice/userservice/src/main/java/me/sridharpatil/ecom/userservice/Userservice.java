3a1cda302eb671755f89a46920d212882838a2c2
sridharpatil047 <sridharpatil047@gmail.com>
2025-01-10 17:27:40 +0530

    initial project setup

---
+++ userservice/userservice/src/main/java/me/sridharpatil/ecom/userservice/Userservice.java
@@ -0,0 +1,13 @@
+package me.sridharpatil.ecom.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Userservice {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Userservice.class, args);
+    }
+
+}
