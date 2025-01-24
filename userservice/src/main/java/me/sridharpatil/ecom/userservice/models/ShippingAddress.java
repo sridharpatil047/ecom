3f872c92b8a1f0289697f48cca490f5b1e16559e
sridharpatil047 <sridharpatil047@gmail.com>
2025-01-24 14:53:01 +0530

    adding Shipping Address to User entity

---
+++ userservice/src/main/java/me/sridharpatil/ecom/userservice/models/ShippingAddress.java
@@ -0,0 +1,21 @@
+package me.sridharpatil.ecom.userservice.models;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class ShippingAddress extends BaseModel {
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String pinCode;
+
+    @ManyToOne
+    private User user;
+
+}
