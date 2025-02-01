885e4e16a2dceedde6af20ff5d5229ae3166d5f3
sridharpatil047 <sridharpatil047@gmail.com>
2025-02-01 20:01:11 +0530

    configuring service discovery - eureka server

---
+++ servicediscovery/src/main/java/me/sridharpatil/ecom/servicediscovery/ServicediscoveryApplication.java
@@ -0,0 +1,15 @@
+package me.sridharpatil.ecom.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServicediscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicediscoveryApplication.class, args);
+    }
+
+}
