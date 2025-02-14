5c92b7bbd5c672ca0635e23f9ed1dc772f5a025b
sridharpatil047 <sridharpatil047@gmail.com>
2025-02-14 13:38:05 +0530

    adding security

--- searchservice/src/main/java/me/sridharpatil/ecom/searchservice/repositories/ProductRepository.java
+++ searchservice/src/main/java/me/sridharpatil/ecom/searchservice/repositories/ProductRepository.java
@@ -1,6 +1,7 @@
 package me.sridharpatil.ecom.searchservice.repositories;
 
 import me.sridharpatil.ecom.searchservice.documents.Product;
+import me.sridharpatil.ecom.searchservice.repositories.customs.ProductCustomRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
