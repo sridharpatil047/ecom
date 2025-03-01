5661e38392ebe9f704afbba0682c2e5ede027537
sridharpatil047 <sridharpatil047@gmail.com>
2025-03-01 17:46:42 +0530

    Updated modified files

--- searchservice/src/test/java/me/sridharpatil/ecom/searchservice/SearchserviceApplicationTests.java
+++ searchservice/src/test/java/me/sridharpatil/ecom/searchservice/SearchserviceApplicationTests.java
@@ -3,11 +3,11 @@ package me.sridharpatil.ecom.searchservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SearchserviceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class SearchserviceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
